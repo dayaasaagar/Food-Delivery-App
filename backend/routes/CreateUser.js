19ff3eb7a64b5ed259edf8d1327fed654e0124d4
dayaasaagar <rdayasaagar@gmail.com>
2024-01-09 15:53:06 -0500

    commit today#1

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User');
+const { validationResult } = require('express-validator');
 
-router.post("/createuser",async (req,res)=>{
+
+router.post("/createuser", body('email').isEmail(),
+body('name').isLength({ min: 5 }),
+body('password').isLength({ min: 5 }),async (req,res)=>{
+
+    const error = validationResult(req);
+    if(!error.isEmpty())
+    {
+        return res.status(400).json({errors:errors.array()});
+    }
     try {
        await User.create({
 
